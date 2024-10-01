8ed19425fd4edd3cba35beff8e8df8ebffc15cfe
Angkit Khadka <khadkaankit85@gmail.com>
2024-10-01 14:12:38 -0400

    all validation is done for signup form except user already exists one

--- src/middlewares/userdata.validation.ts
+++ src/middlewares/userdata.validation.ts
@@ -1,16 +1,31 @@
-import { Request,Response,NextFunction } from "express"
-import { check, validationResult} from "express-validator"
+import { Request, Response, NextFunction } from "express";
+import { check, validationResult } from "express-validator";
 
-// express validator chain for form data validation
-export const signUpDataValidationMiddleware=[
- check("username").isString().withMessage("not a valid username").isLength({min:5,max:15}).withMessage("invalid lenght of username") ,
- (req:Request,res:Response,next:NextFunction)=>{
-const error=validationResult(req)
-    if(!error.isEmpty()){
-        res.status(422).json({errors:error.array()});
+// array of validator middlewares:)
+export const signUpDataValidationMiddleware = [
+  //array of  middleware to validate different data
+  check("username")
+    .isString()
+    .withMessage("Invalid Username datatype")
+    .isLength({ min: 5, max: 15 })
+    .withMessage("Invalid username length"),
+
+  check("password")
+    .isLength({ min: 4 })
+    .withMessage("Min length of password is 4"),
+  check("email").isEmail().withMessage("Must be a valid email"),
+
+  check("phone").optional().isLength({max:10,min:10}).withMessage("Must be 10 in length"),
+
+  //second middleware to check for the result of the query of previous validations:)
+  (req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      res.status(422).json({ errors: error.array() });
+      return;
     }
-    next()
- }
-]
+    next();
+  },
+];
 
-export const loginDataValidationMiddleware=[]
+export const loginDataValidationMiddleware = [];
