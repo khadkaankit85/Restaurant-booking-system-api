53aff3b3955f07d78adfeb17ec5d56926ce65016
Angkit Khadka <khadkaankit85@gmail.com>
2024-11-06 18:30:26 -0500

    routers for the table and reservation endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import express from "express";
-import userRouter from "./Routes/user.routes"
+import userRouter from "./Routes/user.routes";
+import tableRouter from "./Routes/table.routes";
+import reservationRouter from "./Routes/reservation.routes";
 
-const app=express()
+const app = express();
 
 //middleware to parse data
-app.use(express.json())
-app.use(express.urlencoded({
-    extended:true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // endpoint for /user
-app.use("/user", userRouter)
+app.use("/user", userRouter);
+
+app.use("/table", tableRouter);
+app.use("/reservation", reservationRouter);
 
 //Routes go here:)
 
-export default app
+export default app;
