2d577151cb794cb1f7051170bd74f74badb123d9
Angkit Khadka <khadkaankit85@gmail.com>
2024-11-06 18:46:29 -0500

    method to get specific table status

--- src/Routes/table.routes.ts
+++ src/Routes/table.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Request, Response } from "express";
-import { getAllTables } from "../services/table.service";
+import { getAllTables, getTableStatus } from "../services/table.service";
+import { table } from "console";
 const router = Router();
 
 router.get("/getTables", (req: Request, res: Response) => {
@@ -11,6 +12,25 @@ router.get("/getTables", (req: Request, res: Response) => {
     .catch((err) => {
       res.status(500).json({ message: err.message });
     });
+});
+
+router.get("/tablestatus?tableID", (req: Request, res: Response) => {
+  const tableID: string = req.query.tableID as unknown as string;
+  if (tableID === undefined) {
+    res.status(400).json({ message: "Table ID not provided" });
+    return;
+  }
+  getTableStatus(parseInt(tableID))
+    .then((table) => {
+      res.json(table);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.post("/bookTable", (req: Request, res: Response) => {
+  const tableID: String = req.body.tableID;
 });
 
 export default router;
--- src/services/table.service.ts
+++ src/services/table.service.ts
@@ -3,3 +3,10 @@ import { prisma } from "../prisma/prismaClient";
 export const getAllTables = async () => {
   return await prisma.table.findMany();
 };
+export const getTableStatus = async (tableID: number) => {
+  return await prisma.table.findUnique({
+    where: {
+      id: tableID,
+    },
+  });
+};
