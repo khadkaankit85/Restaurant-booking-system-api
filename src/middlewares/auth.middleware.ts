2ebb781a75fb8c4059cd72da26734e6db3a98d23
Angkit Khadka <khadkaankit85@gmail.com>
2024-11-09 01:16:13 -0500

    role based authentication

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -5,7 +5,16 @@ import { UserRequest } from "../types/extras";
 
 dotenv.config();
 
+/**
+ * Verify the JWT token sent in the Authorization header
+ * @param {string} [role="user"] - The role of the user to verify
+ * @param {Request} req - The request object
+ * @param {Response} res - The response object
+ * @param {NextFunction} next - The next middleware in the stack
+ * @throws {401} - If the token is invalid or the user is not authorized
+ */
 export const verifyJWT = async (
+  role = "user",
   req: UserRequest,
   res: Response,
   next: NextFunction
@@ -18,10 +27,14 @@ export const verifyJWT = async (
       token,
       process.env.JWT_ACCESS_TOKEN_SECRET as string,
       (err: Error | null, decoded: JwtPayload | string | undefined) => {
-        if (err) return res.status(401).json({ message: "forbidden" });
+        if (err) return res.status(401).json({ message: "unauthorised" });
         if (decoded && typeof decoded == "object" && decoded.UserInfo) {
+          const roleFromJwt = decoded.UserInfo.role;
+          if (roleFromJwt !== role) {
+            return res.status(401).json({ message: "unauthorised" });
+          }
           req.username = decoded.UserInfo.username;
-          req.role = decoded.UserInfo.role;
+          req.role = role;
           next();
         }
       }
