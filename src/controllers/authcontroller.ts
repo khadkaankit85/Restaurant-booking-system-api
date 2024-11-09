f3aa1dddcc0cd649296273b563624a322f17269e
Angkit Khadka <khadkaankit85@gmail.com>
2024-11-09 00:37:11 -0500

    login logout and refresh function with jwt

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -1,8 +1,8 @@
 import asyncHandler from "express-async-handler";
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import { finduserWithUsername } from "../services/user.services";
 import { authUserWithPassword } from "./user.controller";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload, VerifyOptions } from "jsonwebtoken";
 
 const login = asyncHandler(async (req: Request, res: Response) => {
   const { username, password } = req.body;
@@ -58,6 +58,53 @@ const login = asyncHandler(async (req: Request, res: Response) => {
   res.json({ accessToken });
 });
 
-const refresh = (req: Request, res: Response) => {};
+const refresh = (req: Request, res: Response) => {
+  const cookies = req.cookies;
 
-const logout = (req: Request, res: Response) => {};
+  if (!cookies?.jwt) return res.status(401).json({ message: "unauthorized" });
+
+  const refreshToken = cookies.jwt;
+
+  jwt.verify(
+    refreshToken,
+    process.env.JWT_ACCESS_TOKEN_SECRET as string,
+    async (err: Error | null, decoded: JwtPayload | string | undefined) => {
+      if (err) return res.status(403).json({ message: "forbidden" });
+
+      if (decoded && typeof decoded == "object") {
+        const foundUser = await finduserWithUsername(decoded.UserInfo.username);
+        if (!foundUser)
+          return res.status(401).json({ message: "unauthorizsed" });
+
+        const accessToken = jwt.sign(
+          {
+            UserInfo: {
+              username: foundUser.username,
+              role: foundUser.role,
+            },
+          },
+          process.env.JWT_ACCESS_TOKEN_SECRET as string,
+          { expiresIn: "10s" }
+        );
+
+        res.json({ accessToken });
+      } else {
+        return res.status(403).json({ message: "Invalid refresh token" });
+      }
+    }
+  );
+};
+
+const logout = (req: Request, res: Response) => {
+  const cookies = req.cookies;
+  if (!cookies.jwt) return res.sendStatus(204);
+  res.clearCookie("jwt", {
+    httpOnly: true, //Flags the cookie to be accessible only by the web server not client
+    secure: true, //https only
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  res.json({ message: "cookie cleared" });
+};
+
+export { login, logout, refresh };
