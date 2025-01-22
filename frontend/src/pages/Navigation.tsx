a73a98c1d66129b2dffd3e3cdc5c6a1515f65b2e
aarati01 <131001442+aarati01@users.noreply.github.com>
2025-01-22 00:14:30 -0500

    little change in the navbar

--- frontend/src/pages/Navigation.tsx
+++ frontend/src/pages/Navigation.tsx
@@ -5,11 +5,12 @@ const Navigation = () => {
     <div className="landingpagediv">
       <div className='insideLanding'>
       <nav className="nav">
-        <img
+        {/* <img
           src="/image/Astronomia.jpg"
           alt="Astronomia Logo"
           className="logo"
-        />
+        /> */}
+        <h1 className='resturantName ml-3  text-cyan-800 hover:text-cyan-600 text-2xl font-serif font-bold'>Hamro Resturant</h1>
         <ul className="flex space-x-4 m-3">
           <li className="li">Menue</li>
           <li className="li">Login</li>
