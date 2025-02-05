db23994e800848fce97bd04788b72823ec010c09
Angkit Khadka <khadkaankit85@gmail.com>
2025-02-05 01:53:30 -0500

    deployment fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "./Restaurant-booking-system-api",
-  base: "./",
+  base: "/Restaurant-booking-system-api/",
+  //base: "/Restaurant-booking-system-api",
+  //base: "./",
 });
